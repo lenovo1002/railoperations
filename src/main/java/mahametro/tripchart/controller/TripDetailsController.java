4d0d65cdff3a68a70711106379ba30e12b583955
lenovo1002 <parsharam.raj.shantaram@gmail.com>
2026-06-26 20:39:00 +0530

    implementing server site rendering

--- src/main/java/mahametro/tripchart/controller/TripDetailsController.java
+++ src/main/java/mahametro/tripchart/controller/TripDetailsController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import mahametro.tripchart.entity.TripDetails;
 import mahametro.tripchart.service.TripChartService;
-@CrossOrigin("https://convinced-parties-championship-intro.trycloudflare.com/")
 @RestController
 @RequestMapping("tripchart")
 public class TripDetailsController {

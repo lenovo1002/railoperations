6d8861fca331a250b1c8a8cda674e099b3b18f27
lenovo1002 <parsharam.raj.shantaram@gmail.com>
2026-07-03 11:51:27 +0530

    cors changes

--- src/main/java/mahametro/tripchart/config/SecurityConfig.java
+++ src/main/java/mahametro/tripchart/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
 
 	    CorsConfiguration configuration = new CorsConfiguration();
 
-	    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+	    configuration.setAllowedOrigins(List.of("https://metroduty.in"));
 	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 	    configuration.setAllowedHeaders(List.of("*"));
 	    configuration.setAllowCredentials(true);
